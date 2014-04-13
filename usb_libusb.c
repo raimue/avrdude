d7250134d28cef4d25593671cb98908ff1adf4ab
Rainer Müller <raimue@codingfarm.de>
2014-04-13 21:56:33 +0200

    Support AVR Xplained Mini
    * avrdude.conf.in: Add AVR Xplained Mini with integrated debugger
    * usbdevs.h: Switch to packet size for mEDBG
    * usb_libusb.c: Do not append zero-length packets in usbdev_send()

--- usb_libusb.c
+++ usb_libusb.c
@@ -168,6 +168,13 @@ static int usbdev_open(char * port, union pinfo pinfo, union filedescriptor *fd)
 		      fd->usb.eep = 0;
 		  }
 
+		  if(strstr(product, "mEDBG") != NULL)
+		  {
+		      /* The AVR Xplained Mini uses different endpoints. */
+		      fd->usb.rep = 0x81;
+		      fd->usb.wep = 0x02;
+		  }
+
 		  if (verbose)
 		    fprintf(stderr,
 			    "%s: usbdev_open(): Found %s, serno: %s\n",
@@ -371,7 +378,7 @@ static int usbdev_send(union filedescriptor *fd, unsigned char *bp, size_t mlen)
     }
     bp += tx_size;
     mlen -= tx_size;
-  } while (tx_size == fd->usb.max_xfer);
+  } while (mlen > 0);
 
   if (verbose > 3)
   {
